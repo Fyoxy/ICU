580e10accdfba58f5db42b44ba08a9c47597cde6
Marti <martikingisepp@gmail.com>
2021-10-13 12:42:56 +0300

    Added comments

--- obj/controller.cpp
+++ obj/controller.cpp
@@ -14,6 +14,7 @@ void ControllerListener( Controller device, Motors motor ) {
     std::cout << "Controller listener started" << std::endl;
     while ( read_event( device.controller, &device.event ) == 0 ) {
 
+        // Get controller input
         switch (device.event.type)
         {
             case JS_EVENT_BUTTON:
@@ -35,9 +36,6 @@ void ControllerListener( Controller device, Motors motor ) {
                 break;
         }
 
-        std::cout << "Throttle: " << device.throttle << std::endl;
-        std::cout << "Reverse: " << device.reverse << std::endl;
-
         if ( ( !device.throttle && !device.reverse ) ) {
             // Kill motors, no throttle or reverse pressed
             motor.SetSpeed( 0 );
@@ -45,28 +43,28 @@ void ControllerListener( Controller device, Motors motor ) {
         else {
             // Converting controller data to acceptable PWM range
             int axis = ( device.throttle ) ? device.axes[device.axis].y : device.axes[device.axis].x;
-
 			
 			// Split CONTROLLER_AXIS_MAX into 50 segments for negative and positive values
 			int divider = CONTROLLER_AXIS_MAX / 50;
 			int temp = 0;
 			int speed;
 
+            // Conversion loop to get speed up to 50
 			for (speed = 0; abs( axis ) >= temp; temp += divider, speed++);
 			
-
+            // Check check if throttle or anything else
             if ( device.throttle ) {
+                // Check if axis was negative or positive to determine speed segment 1 - 50 or 51 - 100
                 speed = ( device.axes[device.axis].y > 0 ) ? ( 49 + speed ) : ( 51 - speed );
                 motor.SetSpeed( speed );
             }
             else {
+                // Same process for reverse
                 speed = ( device.axes[device.axis].x > 0 ) ? ( 49 + speed ) : ( 51 - speed );
+                // Invert speed to reverse
                 motor.SetSpeed( -speed );
             }
-
         }
-
-
         fflush(stdout);
     }
 
