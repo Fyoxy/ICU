b2397299ccdff5b214baf2609bde8643899738e4
Marti <martikingisepp@gmail.com>
2021-10-19 16:54:49 +0300

    Debugging

--- obj/main.cpp
+++ obj/main.cpp
@@ -48,8 +48,8 @@ void ControllerListener( Controller* device, Motors* motor ) {
             std::cout << "Axis" << std::endl;
             std::cout << device->axes[device->axis].y << std::endl;
             std::cout << "Axis2" << std::endl;
-            int axis = ( device->throttle ) ? device->axes[device->axis].y : device->axes[device->axis].x;
-			
+            int axis = ( device->throttle ) ? device->axes[device->axis].x : device->axes[device->axis].x;
+
             std::cout << "Divider/speed" << std::endl;
 			// Split CONTROLLER_AXIS_MAX into 50 segments for negative and positive values
             // DS4 Controller sends values from -32767 to 32767
