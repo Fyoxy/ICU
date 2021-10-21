bf3cbfa3b40b5697fd49c1ea810df0d5b8da1856
Marti <martikingisepp@gmail.com>
2021-10-21 12:42:06 +0300

    Forgot to loop driving thread

--- obj/main.cpp
+++ obj/main.cpp
@@ -7,65 +7,67 @@
 
 void ManualDriving( Controller* device, Motors* motor ) {
 
-    // If input throttle or reverse
-    if ( ( !( device->event.number == device->ButtonType::Throttle ) &&
-           !( device->event.number == device->ButtonType::Reverse ) ) ) {
+    for (; motor->GetControlType() == Motors::Manual; ) {
+        // If input throttle or reverse
+        if ( ( !( device->event.number == device->ButtonType::Throttle ) &&
+            !( device->event.number == device->ButtonType::Reverse ) ) ) {
 
-               std::cout << "Kill motors" << std::endl;
+                std::cout << "Kill motors" << std::endl;
 
-        // Kill motors, no throttle or reverse pressed
-        motor->SetSpeed( 0 );
-    }
-    else {
-        std::cout << "Else motors" << std::endl;
-        // Get axis state because otherwise it may get button pressed before axis
-        device->axis = get_axis_state(&device->event, device->axes);
-
-        // Converting controller data to acceptable PWM range
-        int axis = ( device->event.number == device->ButtonType::Throttle ) ?
-            device->axes[device->axis].y : device->axes[device->axis].x;
-
-        // Split CONTROLLER_AXIS_MAX into 50 segments for negative and positive values
-        // DS4 Controller sends values from -32767 to 32767
-        int divider = CONTROLLER_AXIS_MAX / 50;
-        int speed = ( int ) abs( axis ) / divider;
-        
-        // Check check if throttle or anything else
-        if ( device->throttle ) {
-            // Check if axis was negative or positive to determine speed segment 1 - 50 or 51 - 100
-            speed = ( device->axes[device->axis].y > 0 ) ? ( 50 + speed ) : ( 51 - speed );
-            motor->SetSpeed( speed );
+            // Kill motors, no throttle or reverse pressed
+            motor->SetSpeed( 0 );
         }
         else {
-            // Same process for reverse
-            speed = ( device->axes[device->axis].x > 0 ) ? ( 50 + speed ) : ( 51 - speed );
-            // Invert speed to reverse
-            motor->SetSpeed( -speed );
-        }
-    }
-    
-    // If steering with left thumbstick
-    if ( device->axis == 0 ) {
-        if ( device->axes[device->axis].x == 0 ) {
-            motor->SetAngle( SERVO_BASE_ANGLE );
-        } 
-        else {
+            std::cout << "Else motors" << std::endl;
+            // Get axis state because otherwise it may get button pressed before axis
+            device->axis = get_axis_state(&device->event, device->axes);
+
             // Converting controller data to acceptable PWM range
-            int axis = device->axes[device->axis].x;
+            int axis = ( device->event.number == device->ButtonType::Throttle ) ?
+                device->axes[device->axis].y : device->axes[device->axis].x;
 
-            // Split CONTROLLER_AXIS_MAX into 200 segments for negative and positive values
-            int divider = CONTROLLER_AXIS_MAX / 200;
-
-            // Conversion loop to get angle up to 200
-            int angle = ( int ) abs( axis ) / divider;
-
-            if ( device->axes[device->axis].x < 0 ) {
-                motor->SetAngle( SERVO_BASE_ANGLE + angle - 1 );
-            }  
-            else if ( device->axes[device->axis].x > 0 ) { 
-                motor->SetAngle( SERVO_BASE_ANGLE - angle + 1 );
+            // Split CONTROLLER_AXIS_MAX into 50 segments for negative and positive values
+            // DS4 Controller sends values from -32767 to 32767
+            int divider = CONTROLLER_AXIS_MAX / 50;
+            int speed = ( int ) abs( axis ) / divider;
+            
+            // Check check if throttle or anything else
+            if ( device->throttle ) {
+                // Check if axis was negative or positive to determine speed segment 1 - 50 or 51 - 100
+                speed = ( device->axes[device->axis].y > 0 ) ? ( 50 + speed ) : ( 51 - speed );
+                motor->SetSpeed( speed );
             }
-        } 
+            else {
+                // Same process for reverse
+                speed = ( device->axes[device->axis].x > 0 ) ? ( 50 + speed ) : ( 51 - speed );
+                // Invert speed to reverse
+                motor->SetSpeed( -speed );
+            }
+        }
+        
+        // If steering with left thumbstick
+        if ( device->axis == 0 ) {
+            if ( device->axes[device->axis].x == 0 ) {
+                motor->SetAngle( SERVO_BASE_ANGLE );
+            } 
+            else {
+                // Converting controller data to acceptable PWM range
+                int axis = device->axes[device->axis].x;
+
+                // Split CONTROLLER_AXIS_MAX into 200 segments for negative and positive values
+                int divider = CONTROLLER_AXIS_MAX / 200;
+
+                // Conversion loop to get angle up to 200
+                int angle = ( int ) abs( axis ) / divider;
+
+                if ( device->axes[device->axis].x < 0 ) {
+                    motor->SetAngle( SERVO_BASE_ANGLE + angle - 1 );
+                }  
+                else if ( device->axes[device->axis].x > 0 ) { 
+                    motor->SetAngle( SERVO_BASE_ANGLE - angle + 1 );
+                }
+            } 
+        }
     }
         
 }
