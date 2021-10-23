22ba3369e7042f92f4d538468c129e89b074bfb3
Marti <martikingisepp@gmail.com>
2021-10-23 13:42:44 +0300

    Test debug

--- obj/main.cpp
+++ obj/main.cpp
@@ -24,6 +24,7 @@ int main() {
             case JS_EVENT_BUTTON:
                 if ( device.event.number == device.ButtonType::T ) {
                     detectionT = std::thread( Detection, motor );
+                    detectionT.join();
                     break;
                 }
                 else if ( device.event.number == device.ButtonType::O ) {
