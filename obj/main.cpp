1eccdd0a55f4ac14256d9fc91b3fa48bf835ef7f
Marti <martikingisepp@gmail.com>
2021-11-05 14:30:38 +0200

    Deubg

--- obj/main.cpp
+++ obj/main.cpp
@@ -65,6 +65,8 @@ void Ultrasonic( Motors* motor ) {
         // Add value to the distance array
         distanceArr[counter] = distanceCm;
 
+        std::cout << distanceCm << std::endl;
+
         // Adjust speed according to distance from object(s)
         if ( distanceCm <= US_MIN_CM ) {
             motor->ultrasonicMultiplier = 0;
